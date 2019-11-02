ae77f1788f86654ff3028d410d79cdd7823d502e
Hao Pham <phamvanhao009@gmail.com>
2019-11-02 15:46:59 +0700

    first commit

---
+++ app/src/main/java/com/thohao/freakingmath_kp12/MainActivity.java
@@ -0,0 +1,40 @@
+package com.thohao.freakingmath_kp12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*Random random=new Random();
+        for (int i=0;i<=30;i++){
+            int number = random.nextInt(max-min+1)+min;
+
+            Log.d("BBB", number + " ");
+        }*/
+
+        //cach xem tham so trong method ctrl+P
+        //ep kieu sang chuỗi string
+        //Toast.makeText(MainActivity.this, String.valueOf(number), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(MainActivity.this, number+"", Toast.LENGTH_SHORT).show();
+
+        //git branch ; tạo nhánh
+        //git reset ; xoa commit
+        //git --amend; gộp commit
+        //git merge ; gọp code từ nhánh khác
+
+
+
+
+
+    }
+}
